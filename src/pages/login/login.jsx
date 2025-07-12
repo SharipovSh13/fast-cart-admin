b68a1ea70334590eca6fa07d34c0064ed16af5e8
shuhrat <shyhratyagnobi2002@gmail>
2025-07-12 23:08:25 +0300

    create login page ui

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,14 +1,12 @@
-import FormControl from "@mui/joy/FormControl";
-import FormLabel from "@mui/joy/FormLabel";
 import Input from "@mui/joy/Input";
 import Button from "@mui/joy/Button";
 
 const Login = () => {
   return (
     <div className="mb-4 ">
-      <div className="flex gap-0.5 items-center">
+      <div className="grid grid-cols-2 ">
         <div
-          className="flex flex-col space-y-0.5 bg-gradient-to-t from-slate-900 to-sky-950 w-[50%] h-screen p-1
+          className="flex flex-col space-y-0.5 bg-gradient-to-t from-slate-900 to-sky-950 w-[100%] h-screen p-1
 justify-center         "
         >
           <h1 className="text-2xl ml-6 text-gray-50 font-medium">
@@ -16,25 +14,22 @@ justify-center         "
           </h1>
           <img src="/Group 1116606595 (1).png" alt="" className="w-62 ml-6" />
         </div>
-        <div className="w-[50%] bg-gray-200 h-screen">
-          <div>
+        <div className="w-[100%] bg-gray-200 h-screen flex">
+          <div className="w-[80%] m-auto flex flex-col space-y-3">
             <label htmlFor="" className="font-medium text-xl md:text-2xl ">
               Log in
             </label>
-            <FormControl className="flex flex-col space-y-3">
-              <FormLabel></FormLabel>
-              <Input placeholder="Email" />
-              <Input placeholder="Password" type="password" />
-              <Button color="" className="text-blue-600 ">
-                Forgot Password?
-              </Button>
-              <Button
-                className="rounded-[12px]
+            <Input placeholder="Email" />
+            <Input placeholder="Password" type="password" />
+            <Button color="" className="text-blue-600 ">
+              Forgot Password?
+            </Button>
+            <Button
+              className="rounded-[12px]
               "
-              >
-                Log in
-              </Button>
-            </FormControl>
+            >
+              Log in
+            </Button>
           </div>
         </div>
       </div>
